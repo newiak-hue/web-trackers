05866a7c1eb8475b2380e2a907a367fc91fff1a3
newiak-hue <teekaiwen36@gmail.com>
2025-11-30 03:19:02 +0800

    Add files via upload
    MySQL database dump for the tracker

---
+++ trackerdb.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 29, 2025 at 08:17 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `trackerdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bitcoin_price`
+--
+
+CREATE TABLE `bitcoin_price` (
+  `id` bigint(20) NOT NULL,
+  `scrape_time` datetime NOT NULL,
+  `price_usd` decimal(20,2) NOT NULL,
+  `high_24h_usd` decimal(20,2) DEFAULT NULL,
+  `low_24h_usd` decimal(20,2) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bitcoin_price`
+--
+ALTER TABLE `bitcoin_price`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bitcoin_price`
+--
+ALTER TABLE `bitcoin_price`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
